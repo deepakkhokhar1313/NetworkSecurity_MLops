78c5bedec93887a6a9858b3cf28f057a1b893c89
deepakkhokhar1313 <deepakkhokhar1313@gmail.com>
2025-01-18 11:53:23 +0000

    Data validation pipeline implemented

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from networksecurity.exceptionHandling.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 import sys
 
 if __name__ == "__main__":
@@ -18,5 +19,20 @@ if __name__ == "__main__":
         )
         dataIngestionArtifact = data_ingestion.initiate_data_ingestion()
         print(dataIngestionArtifact)
+        logging.info(
+            "Data Ingestion completed."
+        )
+        
+
+        data_validation_config = DataValidationConfig(trainingPipelineConfig)
+        data_validation = DataValidation(dataIngestionArtifact,data_validation_config)
+        logging.info(
+            "Data Validation started."
+        )
+        data_validation_artifacts = data_validation.initiate_data_validation()
+        logging.info(
+            "Data Validation completed."
+        )
+        print(data_validation_artifacts)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -7,3 +7,12 @@ Dataclasses are used as a decorator to make an empty class like without funtions
 class DataIngestionArtifact:
     trained_file_path:str
     test_file_path:str
+
+@dataclass
+class DataValidationArtifacts:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
